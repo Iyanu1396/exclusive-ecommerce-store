80aa881904a147a05c6b4aef7aa6f8e8db2cdf2c
Iyanu Oluwa <iyanu1396@gmail.com>
2024-07-14 02:23:38 +0100

    footer mobile ui (change made)

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,12 +1,24 @@
+import { AiOutlineSend } from "react-icons/ai";
+
 function Footer() {
   return (
-    <footer className="bg-slate-950 text-slate-100 pt-12 pb-6 font-inter">
-      <div className="m-auto grid w-[80%] grid-cols-2 gap-3">
+    <footer className="bg-slate-950 text-slate-100 pt-12 pb-6 font-inter text-[13px]">
+      <div className="m-auto grid w-[80%] grid-cols-2 gap-6">
         <div className="flex flex-col space-y-2">
           <small className="text-lg font-semibold">Exclusive</small>
           <small>Subscribe</small>
           <small>Get 10% off your first order</small>
-          <small>Enter your email</small>
+          <small className=" flex w-[120px]   items-center border-2  rounded-sm ">
+          <input
+            className="font-mont w-[100px] pl-2  h-8 bg-slate-950  outline-none "
+            placeholder="Enter your email"
+            type="text"
+          />
+          <button>
+          <AiOutlineSend
+           className="text-slate-100" />
+          </button>
+        </small>
         </div>
 
         <div className="flex flex-col space-y-2">
